8ffd57f28c4311d70c898a78a3191e10d7baa272
dani <dani.zaghian@gmail.com>
2021-08-25 14:04:41 -0700

    adds url segment params

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get "/query_params", controller: "params_examples", action: "query_params"
   get "/guess_game", controller: "params_examples", action: "guess_game"
   get "/url_segment_params/:wildcard", controller: "params_examples", action: "url_segment_params"
+  get "/guess_game/:guess", controller: "params_examples", action: "guess_game"
 end
