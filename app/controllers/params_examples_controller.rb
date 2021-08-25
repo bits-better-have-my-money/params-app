29297f4068a4a37b156cf01cd3637c11e6a11e82
dani <dani.zaghian@gmail.com>
2021-08-25 12:32:14 -0700

    adds name solution

--- app/controllers/params_examples_controller.rb
+++ app/controllers/params_examples_controller.rb
@@ -1,9 +1,18 @@
 class ParamsExamplesController < ApplicationController
 
   def query_params
-    user_name = params["name"]
-    user_message = params[:message]
-    render json: {message: "The name is #{user_name}, and the message is #{user_message}"}
+    user_name = params["name"].upcase
+    if user_name.starts_with?("A")
+      message = "Hey, your name starts with the first letter of the alphabet!"
+    else
+      message = "Your name is #{user_name}"
+    end
+    render json: {message: message}
+  end
+
+  def guess_game
+    input_guess = params[:guess]
+    render json: {message: "You guessed #{input_guess}"}
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get "/query_params", controller: "params_examples", action: "query_params"
+  get "/guess_game", controller: "params_examples", action: "guess_game"
 end
