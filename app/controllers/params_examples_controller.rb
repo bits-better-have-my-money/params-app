681e22928c15d39a22a229604d2eeb90c9ae3c71
dani <dani.zaghian@gmail.com>
2021-08-25 12:03:03 -0700

    adds query parameter example

--- app/controllers/params_examples_controller.rb
+++ app/controllers/params_examples_controller.rb
@@ -1,2 +1,9 @@
 class ParamsExamplesController < ApplicationController
+
+  def query_params
+    user_name = params["name"]
+    user_message = params[:message]
+    render json: {message: "The name is #{user_name}, and the message is #{user_message}"}
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/query_params", controller: "params_examples", action: "query_params"
 end
